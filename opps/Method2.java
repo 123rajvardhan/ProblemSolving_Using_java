e1a8b43c4158b23e5ee971ad66ea4c8314b313cc
vardhanbyteiq <vardhan.byteiq@gmail.com>
2026-02-23 22:00:40 +0530

    intial commit

---
+++ opps/Method2.java
@@ -0,0 +1,10 @@
+public class Method2 
+{
+    
+          public void raj()
+          {
+             System.out.println("hello ");
+          }
+    
+        
+}
---
+++ opps/Methods.java
@@ -0,0 +1,10 @@
+public class Methods 
+{
+   public static void main(String[] args)
+   {
+       int ch='c';
+          
+       System.out.println((int)ch);
+   }    
+}
+
